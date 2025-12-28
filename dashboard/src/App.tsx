2614416a14d5108b4c1ca3529f85ee017e0cb4ac
Jocelyn Lucia Amador Martinez <183007558+Joss1203@users.noreply.github.com>
2025-12-28 13:01:39 -0600

    encabezado2

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -104,7 +104,7 @@ export default function App() {
   {/* IMAGEN */}
   <div style={{ flex: 1, textAlign: "right" }}>
     <img 
-      src="/monitor_clinico.jpg" 
+      src="/monitor-clinico.jpg" 
       alt="Monitor"
       style={{
         maxWidth: "100%",
